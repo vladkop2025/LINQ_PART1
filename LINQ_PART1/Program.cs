48a5c38a7369c5f75d5350c10e9385d762607931
vladkop2025 <vladkop@mail.ru>
2025-04-18 22:22:05 +0300

    Add project files.

---
+++ LINQ_PART1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//Профессия C#-разработчик Язык C# Модуль 14. Основы LINQ.Часть 1
+namespace LINQ_PART1
+{    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
